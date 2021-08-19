8700c3c8b2ab1ad17f2ce23402ace06609261458
GoldenGuy1000 <GoldenGuy1000Contact@gmail.com>
2021-08-19 13:11:50 -0700

    Add project files.

---
+++ Unified Items/Patches/Patches.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace Unified_Items.Patches
+{
+    [HarmonyPatch]
+    public static class Patches
+    {
+
+    }
+}
